d394dc0a9be6d0dade832a1ecceb0b24fed20b2f
Ketki Pramod Randhir <ketki.randhir@gmail.com>
2018-03-15 15:40:47 +0530

    getstudentsrecords

--- python project/StudentReportCard/AdminInfo/views.py
+++ python project/StudentReportCard/AdminInfo/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from AdminInfo.models import StudentDetail
 from django.http import HttpResponse
+import json
+#from django.http import JsonResponse
+#from serpy import Serializer, IntField, StrField, MethodField
 from django.views.generic import View
 def getStudentsRecords(request):
-	student_records= StudentDetail.objects.all()
-	context = {
-	'student_records': student_records
-	}
-	return render(request,'AdminHome.html',context)
+	student_records= StudentDetail.objects.all().values()
+	#student_list = list(student_records)
+	#context = {'key':student_records}
+	render(request,'AdminInfo/AdminHome.html',student_records)
+	#print('bcjsabncdkjq')
+	#print(student_list)
+	#print(JsonResponse(student_list,safe=False))
