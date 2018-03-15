0d75652ab2071e11e7c528ce8e7217bce58e3b91
Ketki Pramod Randhir <ketki.randhir@gmail.com>
2018-03-15 13:35:41 +0530

    student retrieval service with db integration

--- python project/StudentReportCard/AdminInfo/urls.py
+++ python project/StudentReportCard/AdminInfo/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     #path('', views.index,name='index'),
-    url(r'getStudentRecords/',views.getStudentsRecords,name='getSutdentsRecords')
+    url(r'getStudentsRecords/',views.getStudentsRecords,name='getStudentsRecords')
 ]
