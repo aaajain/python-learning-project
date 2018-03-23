21ebefa7cd283e17e42b38cf97271d988bf736bf
CIS\NJadhav <NJadhav@chenoainc.com>
2018-03-23 15:57:31 +0530

    creating global singleton instance on project load in wsgi file

--- python project/StudentReportCard/StudentReportCard/wsgi.py
+++ python project/StudentReportCard/StudentReportCard/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+from AdminInfo.views import SingleToneCollege
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StudentReportCard.settings")
 
 application = get_wsgi_application()
+
+global college
+college= SingleToneCollege.__new__(SingleToneCollege,'MET','Bandra','Mumbai University') 
